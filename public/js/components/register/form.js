6c61d5969abf65fe8b43104d94f6eb8ba372cebd
miguelonga <miguelmundiaragones@gmail.com>
2017-07-12 18:08:36 +0200

    next button shows next page

--- public/js/components/register/form.js
+++ public/js/components/register/form.js
@@ -3,12 +3,19 @@ Class('Register.Form', {
     Extends: Component,
 
     initialize: function() {
-        Register.Form.Super.call(this, 'formulary');
-        this.element.addEventListener('nextPage', this.navigate.bind(this))
+        Register.Form.Super.call(this, 'formulary')
+        this.element.addEventListener('nextPage', this.nextPage.bind(this))
+        this.firstPage = document.getElementById('first-page')
+        this.secondPage = document.getElementById('second-page')
     },
 
-    navigate: function(pageIndex) {
-      console.log(pageIndex.detail)
+    nextPage: function(event) {
+      var pageIndex = event.detail
+      var nextPageIndex = pageIndex + 1
+      var pages = [this.firstPage, this.secondPage]
+
+      pages[pageIndex].show = false
+      pages[nextPageIndex].show = true
     }
 
 });
